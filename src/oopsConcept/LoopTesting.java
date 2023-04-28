61c4b35588ca97ef96399fe4481a7f5c7caecf37
Priyanka Meghwal <p.meghwal1211@gmail.com>
2023-04-29 00:50:47 +0530

    for loop tested

--- src/oopsConcept/LoopTesting.java
+++ src/oopsConcept/LoopTesting.java
@@ -2,6 +2,15 @@ package oopsConcept;
 
 public class LoopTesting {
     public static void main(String[] args) {
-        
+
+
+        for(int i=0;i<=10;i++)
+        {
+            System.out.println(i);
+            int p=i+10;
+            System.out.println(p);
+
+        }
+
     }
 }
